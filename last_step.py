9711fc8f043dba571ad402c104143ef498ea9905
Kirill <89633993+22404042452453@users.noreply.github.com>
2022-02-09 21:30:11 +0300

    Add files via upload

---
+++ last_step.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlite3
+
+###download_information
+with sqlite3.connect("server_db.db") as my_data:
+    sql = my_data.cursor()
+    df = pd.read_excel("провода.xlsx")
+    for i in range(len(df["mark"])):
+        sql.execute(f"SELECT mark_provod FROM provod WHERE mark_provod = '{df['mark'][i]}'")
+        if sql.fetchone() is None:
+            sql.execute(f"INSERT INTO provod VALUES(?,?,?)", (df["mark"][i], df["r0"][i], df["x0"][i]))
+            my_data.commit()
+        else:
+            print("Этот провод уже имеется")
