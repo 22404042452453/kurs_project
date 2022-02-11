fb8b171d140fea7f0db7e5422dfb92e87ee924d6
Kirill <89633993+22404042452453@users.noreply.github.com>
2022-02-11 23:57:13 +0300

    Add files via upload

---
+++ first_matrix.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def first_matrix(model):
+    M = np.zeros(shape=[len(model)+1, len(model)])
+    start, end = [int(i.split('-')[0]) for i in model], [int(i.split('-')[1]) for i in model]
+    try:
+        for i in range(len(start)):
+            M[start[i]-1,i] = 1
+            M[end[i]-1, i] = -1
+
+        M = M[1:,:].astype(np.int32)
+        return np.linalg.inv(M)
+    except Exception as ex:
+
+        return "Something_wrong"
+
+def main():
+    model = input().split()
+    print(first_matrix(model))
+
+if __name__ == '__main__':
+    main()
+
+
