3eec00e5cd06b689cda0c324aa4bd4e66f3eb7de
Kirill <89633993+22404042452453@users.noreply.github.com>
2022-02-13 20:52:03 +0300

    Add files via upload

---
+++ vetvi.py
@@ -0,0 +1,36 @@
+import sqlite3
+import numpy as np
+
+
+def vetvi():
+    R = []
+    X = []
+    with open("линии.txt", 'r', encoding="utf-8") as f:
+        with sqlite3.connect("server_db.db") as my_data:
+            sql = my_data.cursor()
+            for i in f.readlines():
+                sql.execute(f"SELECT r0,x0 FROM provod WHERE mark_provod = '{i.strip().split()[1]}'")
+                if sql.fetchone() is None:
+                    sql.execute(f"SELECT rt,xt FROM trans2 WHERE mark_trans = '{i.strip().split()[1]}'")
+                    r, x = map(float, sql.fetchone())
+                    R.append(r)
+                    X.append(x)
+                else:
+                    sql.execute(f"SELECT r0,x0 FROM provod WHERE mark_provod = '{i.strip().split()[1]}'")
+                    r, x = map(float, sql.fetchone())
+                    length = float(i.strip().split()[2])
+                    R.append(r * length)
+                    X.append(x * length)
+
+    Z = np.zeros(shape=(len(R), len(X))).astype(complex)
+    np.fill_diagonal(Z, [complex(R[i], X[i]) for i in range(len(X))])
+
+    return Z
+
+
+def main():
+    print(vetvi())
+
+
+if __name__ == "__main__":
+    main()
